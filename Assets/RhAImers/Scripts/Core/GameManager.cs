1a750cbcb8ebb8c7dfec373b4fbe043911d15386
tkgpoyo <yktsota1126@gmail.com>
2026-06-24 18:42:53 +0900

    GameManagerを仮実装

--- Assets/RhAImers/Scripts/Core/GameManager.cs
+++ Assets/RhAImers/Scripts/Core/GameManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Codice.Client.Common.GameUI;
 using RhAImers.Battle;
 using RhAImers.Input;
 using RhAImers.Scoring;
@@ -14,8 +16,14 @@ namespace RhAImers.Core
     public class GameManager : MonoBehaviour
     {
         private BattleSession _currentSession;
+        /// <summary>今のターンのバトルコンテキスト</summary>
+        private BattleContext _currentContext;
         private IVerseGenerationService _verseGenerationService;
         private ScoreCalculator _scoreCalculator;
+        /// <summary>ライム入力残り時間(msec)</summary>
+        private float _remainRhymeInputTimeMs;
+        /// <summary>入力されたライムのリスト</summary>
+        private IReadOnlyList<string> _submittedRhymes;
 
         [SerializeField] private RhymeInputController _rhymeInputController;
         [SerializeField] private UIManager _uiManager;
@@ -24,11 +32,39 @@ namespace RhAImers.Core
         public Verse CurrentOpponentVerse { get; private set; }
         public BattleSettings CurrentSettings { get; private set; }
 
-        public GameManager(IVerseGenerationService verseGenerationService, ScoreCalculator scoreCalculator)
+        private void Awake()
         {
-            _verseGenerationService = verseGenerationService;
-            _scoreCalculator = scoreCalculator;
+            _verseGenerationService = new FixedVerseGenerationService();
+            _scoreCalculator = new ScoreCalculator(new(new()), new(new(), new(new RhymeDictionary(new()))));        // TODO: 仮実装のためちゃんと実装
             CurrentState = GameState.Title;
+        }
+
+        private void Update()
+        {
+            switch (CurrentState) {
+                case GameState.Title:
+                    break;
+                case GameState.ModeSelect:
+                    break;
+                case GameState.BattleStart:
+                    break;
+                case GameState.OpponentVerse:
+                    UpdateOpponentVerse();
+                    break;
+                case GameState.RhymeInput:
+                    UpdateRhymeInput();
+                    break;
+                case GameState.VerseGeneration:
+                    UpdateVerseGeneration();
+                    break;
+                case GameState.TurnEnd:
+                    UpdateTurnEnd();
+                    break;
+                case GameState.Scoring:
+                    break;
+                case GameState.Result:
+                    break;
+            }
         }
 
         public void StartGame()
@@ -45,8 +81,17 @@ namespace RhAImers.Core
 
         public void StartTurn()
         {
-            CurrentState = GameState.OpponentVerse;
-            CurrentOpponentVerse = _verseGenerationService.GenerateOpponentVerse(_currentSession.GenerateBattleContext());
+            // 相手バースの生成
+            _currentContext = _currentSession.GenerateBattleContext();                                  // バトルコンテキストの生成
+            CurrentState = GameState.OpponentVerse;                                                     // ゲーム状態を「相手バース生成中」に変更
+            CurrentOpponentVerse = _verseGenerationService.GenerateOpponentVerse(_currentContext);      // 相手バースを生成する
+            _uiManager.ShowOpponentVerse(CurrentOpponentVerse);                                         // 相手バースを表示
+
+            // ライムの入力受付開始
+            _uiManager.ShowInputTimer(CurrentSettings.InputTimeLimitSec);                               // ライム入力タイマーを表示
+            _remainRhymeInputTimeMs = CurrentSettings.InputTimeLimitSec * 1000;                         // ライム入力残り時間を初期化
+            _rhymeInputController.StartInput();                                                         // ライムの入力受付開始
+            CurrentState = GameState.RhymeInput;                                                        // ゲーム状態を「ライム入力中」に変更
         }
 
         public void SubmitRhymes(IReadOnlyList<string> rhymes)
@@ -59,12 +104,10 @@ namespace RhAImers.Core
 
         public void EndTurn()
         {
-            if (_currentSession.IsFinalTurn())
-            {
+            if (_currentSession.IsFinalTurn()) {
                 CurrentState = GameState.Result;
             }
-            else
-            {
+            else {
                 CurrentState = GameState.TurnEnd;
             }
         }
@@ -73,5 +116,42 @@ namespace RhAImers.Core
         {
             CurrentState = GameState.Result;
         }
+
+        #region Updateメソッド内の処理
+        private void UpdateOpponentVerse()
+        {
+            // 相手バースの生成が完了したら，ライム入力受付に移行する
+            CurrentState = GameState.RhymeInput;
+        }
+        private void UpdateRhymeInput()
+        {
+            _remainRhymeInputTimeMs -= Time.deltaTime * 1000;
+            if (_remainRhymeInputTimeMs <= 0) {
+                _submittedRhymes = _rhymeInputController.Submit();
+                CurrentState = GameState.VerseGeneration;
+            }
+        }
+        private void UpdateVerseGeneration()
+        {
+            var playerVerse = _verseGenerationService.GeneratePlayerVerse(_submittedRhymes, CurrentOpponentVerse.Text);
+            var turnData = new TurnData(_currentSession.CurrentTurnIndex, CurrentOpponentVerse, new List<string>(_submittedRhymes), playerVerse);
+            _currentSession.AddTurn(turnData);
+            _uiManager.ShowGeneratedVerse(playerVerse);
+            CurrentState = GameState.TurnEnd;
+        }
+        private void UpdateTurnEnd()
+        {
+            if (_currentSession.IsFinalTurn()) {
+                CurrentState = GameState.Result;
+            }
+            else {
+                _uiManager.ShowScoringLoading();
+                var scores = _scoreCalculator.Calculate(_currentSession.Turns);
+                var result = new BattleResult(_currentSession.Turns, scores);
+                _uiManager.ShowResult(result);
+                CurrentState = GameState.OpponentVerse;
+            }
+        }
+        #endregion (Updateメソッド内の処理)
     }
 }
