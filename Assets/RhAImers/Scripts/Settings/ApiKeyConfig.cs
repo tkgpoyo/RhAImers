6c2bb70c8a34731cd6152e60bba3edf81b10d53a
tkgpoyo <yktsota1126@gmail.com>
2026-07-04 16:16:47 +0900

    APIキーの管理方法を変更

--- Assets/RhAImers/Scripts/Settings/ApiKeyConfig.cs
+++ Assets/RhAImers/Scripts/Settings/ApiKeyConfig.cs
@@ -5,6 +5,8 @@ public class ApiKeyConfig : ScriptableObject
 {
     [SerializeField]
     private string _apiKey;
-
     public string ApiKey => _apiKey;
+
+    private static ApiKeyConfig _instance;
+    public static ApiKeyConfig Instance => _instance ??= Resources.Load<ApiKeyConfig>("Configs/ApiKeyConfig");
 }
