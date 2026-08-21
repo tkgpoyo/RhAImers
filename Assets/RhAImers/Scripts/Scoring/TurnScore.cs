7058dd29a3d8ff4eb85ce124342f7853079e7aa6
tkgpoyo <tkgpoyo@gmail.com>
2026-08-21 20:44:54 +0900

    韻の長さを得点に考慮

--- Assets/RhAImers/Scripts/Scoring/TurnScore.cs
+++ Assets/RhAImers/Scripts/Scoring/TurnScore.cs
@@ -7,15 +7,20 @@ namespace RhAImers.Scoring
         public int RhymeCount { get; }
         public float AverageHardness { get; }
         public int RelevanceCount { get; }
+        // ADD 2026/08/21 ota ライムの平均長さを追加
+        public float AverageLength { get; }
         /// <summary>合計点数</summary>
         /// TODO: スコア計算暫定
-        public int Total => Mathf.FloorToInt(RhymeCount * AverageHardness * (RelevanceCount+1));
+        //public int Total => Mathf.FloorToInt(RhymeCount * AverageHardness * (RelevanceCount+1));
+        public int Total => Mathf.FloorToInt(RhymeCount * AverageHardness * AverageLength);
 
-        public TurnScore(int rhymeCount, float averageHardness, int relevanceCount)
+        // ADD 2026/08/21 ota ライムの平均長さを追加
+        public TurnScore(int rhymeCount, float averageHardness, int relevanceCount, float averageLength)
         {
             RhymeCount = rhymeCount;
             AverageHardness = averageHardness;
             RelevanceCount = relevanceCount;
+            AverageLength = averageLength;
         }
     }
 }
